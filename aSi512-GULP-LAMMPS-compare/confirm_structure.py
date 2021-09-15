2a4fbca70a33e3c652cad7436e7e40a358b04071
Emi Minamitani <eminamitani@ims.ac.jp>
2021-09-15 14:47:03 +0900

    remove too many test input/output

--- aSi512-GULP-LAMMPS-compare/confirm_structure.py
+++
@@ -1,8 +0,0 @@
-from ase.io import read
-
-xyz=read('gulp.xyz',format='xyz')
-vasp=read('optimized.vasp',format='vasp')
-import numpy as np
-diff=np.sum(np.abs(xyz.positions-vasp.positions))
-
-print(diff)
--- aSi512-GULP-LAMMPS-compare/make_LAMMPS_data.py
+++
@@ -1,3 +0,0 @@
-from ase.io import read,write
-vasp=read('optimized.vasp',format='vasp')
-write('optimized.data',vasp,format='lammps-data')
