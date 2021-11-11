0fcf70cb25124052dc06c2c2de7381c52849a839
Emi Minamitani <eminamitani@ims.ac.jp>
2021-11-11 19:20:15 +0900

    track all notebook

---
+++ aSi512_test/dsf_1728.py
@@ -0,0 +1,40 @@
+from ase.io import read
+from tqdm import tqdm
+
+atoms=read('/home/emi/lammps/aSi-1728/relaxed.data',format='lammps-data',style='atomic')
+import numpy as np
+natom=len(atoms.positions)
+nmodes=natom*3
+print('loading data...')
+dyn_file='/home/emi/lammps/aSi-1728/Dyn.form'
+lammps_dyn=np.loadtxt(dyn_file).reshape((nmodes,nmodes))
+eigenvalue, eigenvector=np.linalg.eigh(lammps_dyn)
+print('finish eigenvector/eigenvalue calculation')
+
+kmin=2.0*np.pi/atoms.cell[0,0]
+kmax=kmin*10
+kvec=np.linspace(kmin,kmax,50)
+n_kpt=len(kvec)
+import pyAF.postprocess
+n_sample=8
+n_modes=natom*3
+C_L=np.zeros((n_kpt,n_modes))
+C_T=np.zeros((n_kpt,n_modes))
+position=atoms.positions
+
+for ik,k in enumerate(tqdm(kvec)):
+    k_vector=pyAF.postprocess.polar_coord_sampling(n_sample,k)
+
+    for imode in range(nmodes):
+        ev=eigenvector[:,imode]
+        tmp_L=np.zeros(n_sample*n_sample)
+        tmp_T=np.zeros(n_sample*n_sample)
+        for i,vec in enumerate(k_vector):
+
+            unit_vector_L=vec/np.linalg.norm(vec)
+            tmp_L[i], tmp_T[i]=pyAF.postprocess.get_Ci_ver2(ev,position,vec)
+        C_L[ik,imode]=np.mean(tmp_L)
+        C_T[ik,imode]=np.mean(tmp_T)
+
+np.save('C_L_1728',C_L)
+np.save('C_T_1728',C_T)
